e475d36cd6dab707592bb9800b8925615b2b1625
wshaow <wshaow@126.com>
2019-05-16 00:01:37 +0800

    增加代码笔记和补充基本形态学知识

--- objection_base/test.py
+++ objection_base/test.py
@@ -1,22 +1,19 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    # mouse callback function
+    def draw_circle(event, x, y, flags, param):
+        if event == cv.EVENT_LBUTTONDBLCLK:
+            cv.circle(img, (x, y), 100, (255, 0, 0), -1)
+
+
+    # Create a black image, a window and bind the function to window
+    img = np.zeros((512, 512, 3), np.uint8)
+    cv.namedWindow('image')
+    cv.setMouseCallback('image', draw_circle)
+    while (1):
+        cv.imshow('image', img)
+        if cv.waitKey(20) & 0xFF == ord('q'):
+            break
+    cv.destroyAllWindows()
--- objection_base/threshold_mtehod.py
+++ objection_base/threshold_mtehod.py
@@ -8,21 +8,29 @@ if __name__ == '__main__':
     # 2、将图片转换为灰度图像
     grey = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     # 对图像进行阈值处理，
+    """
+    threshold 函数
+    也就是图像的二值化
+    
+    """
     retval, grey = cv2.threshold(grey, 90, 255, cv2.THRESH_BINARY_INV)
     cv2.imshow('after_threshold', grey)
     cv2.waitKey()
 
-    grey = cv2.erode(grey, None)
+    grey = cv2.erode(grey, None)  # 腐蚀操作把细小的区域去除
     cv2.imshow('after_erode', grey)
     cv2.waitKey()
-    grey = cv2.dilate(grey, None)
+    grey = cv2.dilate(grey, None)  # 膨胀操作将找到的区域变大了
     cv2.imshow('after_dilate', grey)
     cv2.waitKey()
 
+    # 轮廓检测是一个大问题，有时间可以好好看一下
     contours, hierarchy = cv2.findContours(grey.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     newimg = np.zeros_like(grey)
     cv2.drawContours(newimg, contours, -1, 255)
     # cv2.drawContours(newimg, contours, 0, 255)
     cv2.imshow('test', newimg)
     cv2.imwrite("processed.jpg", newimg)
+
     cv2.waitKey()
+    cv2.destroyAllWindows()
