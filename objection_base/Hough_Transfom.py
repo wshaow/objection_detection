b2ec775cdef7e14bfd094217ca9f34411d9ac0db
wshaow <wshaow@126.com>
2019-05-17 23:35:17 +0800

    广义霍夫变换

--- objection_base/Hough_Transfom.py
+++ objection_base/Hough_Transfom.py
@@ -1,8 +1,24 @@
-import cv2 as cv
+import cv2.cv2 as cv
 import numpy as np
+import os
 
 if __name__ == '__main__':
-    img = cv.imread('dave.jpg')
+    img = cv.imread('lines.png')
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray, 50, 150, apertureSize=3)
+    edges = cv.Canny(gray, 75, 150)
+    lines = cv.HoughLinesP(edges, 1, np.pi/180, 50, maxLineGap=50)
+
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+    cv.imshow('ori_image', img)
+    cv.imshow('gray_image', gray)
+    cv.imshow('canny', edges)
+
+    cv.waitKey()
+    cv.destroyAllWindows()
+
+
 
